f32c5fae3e89baefeeff77c7fd5d0d46329b1897
JuanSicardoAT <juan.sicardo@agilethought.com>
2021-05-27 18:01:13 -0500

    Refactorizé Main.sh

--- Main.sh
+++ Main.sh
@@ -1,77 +1,107 @@
 #!/bin/bash
 
 TITLE="==== JediAnalytics Demo ===="
-prompt="--Por favor eliga una opción: "
-response="--Seleccionaste "
-goodbye="¡Nos vemos pronto! :)"
-confyes="--Ejecutando"
-confno="--Regresando"
 
-function new_page () {
-    clear
-    echo "$TITLE"
+PROMPT_SELECT="--Por favor elija una opción: "
+SELECT_CONFIRMATION_MESSAGE="--Seleccionó: "
+INVALID_OPTION_MESSAGE="--Opción inválida... Saliendo"
+MENU_OPTIONS=("Subir archivos a HDFS" "Borrar archivos de HDFS"
+  "Crear base de datos en Hive" "Borrar base de datos en Hive"
+  "Crear tablas externas en Hive" "Borrar tablas externas en Hive"
+  "Crear tablas internas en Hive" "Borrar tablas internas en Hive"
+  "Ejecutar un HQL" "Ejecutar todos los HQLs" "Salir")
+
+GOODBYE_MESSAGE="--Saliendo"
+
+PROMPT_CONFIRMATION="--¿Está seguro?"
+YES_CONFIRMATION_MESSAGE="--Ejecutando"
+NO_CONFIRMATION_MESSAGE="--Saliendo"
+CONFIRMATION_OPTIONS=("Si" "No")
+
+function showNewPage() {
+  clear
+  echo "$TITLE"
 }
 
-confirmations=("Si" "No")
-function confirmation () {
-    echo "--¿Está seguro?"
-    select conf in "${confirmations[@]}"
-    do
-        case $conf in
-            "${confirmations[0]}")
-                 echo "$confyes"
-                 ;;
-            "${confirmations[1]}")
-                 echo "$confno"
-                 exit
-                 ;;
-esac
-done
-}
+function askConfirmation() {
+  echo "$PROMPT_CONFIRMATION"
+  select conf in "${CONFIRMATION_OPTIONS[@]}"; do
+    case $conf in
 
-new_page
+    "${CONFIRMATION_OPTIONS[0]}")
+      echo "$YES_CONFIRMATION_MESSAGE"
+      ;;
 
-PS3="$prompt"
-options=("Subir archivos a HDFS" "Borrar archivos de HDFS" "Crear base de datos en Hive" "Borrar base de datos en Hive" "Crear tablas externas en Hive" "Borrar tablas externas en Hive" "Crear tablas internas en Hive" "Borrar tablas internas en Hive" "Ejecutar un HQL" "Ejecutar todos los HQLs" "Salir" )
-select opt in "${options[@]}"
-do
-    case $REPLY in
-        1)
-            new_page
-            echo "$response$opt"
-            confirmation
-            ;;
-        2)
-            new_page
-            echo "$response$opt"
-            ;;
-        3)
-            echo "$response$opt"
-            ;;
-	4)
-            echo "$response$opt"
-            ;;
-	5)
-            echo "$response$opt"
-            ;;
-	6)
-            echo "$response$opt"
-            ;;
-	7)
-            echo "$response$opt"
-            ;;
-	8)
-            echo "$response$opt"
-            ;;
-	9)
-            echo "$response$opt"
-            ;;
-        10)
-            echo "$response$opt"
-	    echo "$goodbye"
-            break
-            ;;
-        *) echo "invalid option $REPLY";;
+    "${CONFIRMATION_OPTIONS[1]}")
+      echo "$NO_CONFIRMATION_MESSAGE"
+      exit
+      ;;
+
     esac
-break
+    break
+  done
+}
+
+showNewPage
+
+PS3="$PROMPT_SELECT"
+select opt in "${MENU_OPTIONS[@]}"; do
+  case $REPLY in
+
+  1)
+    showNewPage
+    echo "$SELECT_CONFIRMATION_MESSAGE$opt"
+    askConfirmation
+    ;;
+
+  2)
+    showNewPage
+    echo "$SELECT_CONFIRMATION_MESSAGE$opt"
+    ;;
+
+  3)
+    echo "$SELECT_CONFIRMATION_MESSAGE$opt"
+    ;;
+
+  4)
+    echo "$SELECT_CONFIRMATION_MESSAGE$opt"
+    ;;
+
+  5)
+    echo "$SELECT_CONFIRMATION_MESSAGE$opt"
+    ;;
+
+  6)
+    echo "$SELECT_CONFIRMATION_MESSAGE$opt"
+    ;;
+
+  7)
+    echo "$SELECT_CONFIRMATION_MESSAGE$opt"
+    ;;
+
+  8)
+    echo "$SELECT_CONFIRMATION_MESSAGE$opt"
+    ;;
+
+  9)
+    echo "$SELECT_CONFIRMATION_MESSAGE$opt"
+    ;;
+
+  10)
+    echo "$SELECT_CONFIRMATION_MESSAGE$opt"
+    ;;
+
+  11)
+    echo "$SELECT_CONFIRMATION_MESSAGE$opt"
+    echo "$GOODBYE_MESSAGE"
+    break
+    ;;
+
+  *)
+    echo "$INVALID_OPTION_MESSAGE"
+    exit
+    ;;
+
+  esac
+  break
 done
