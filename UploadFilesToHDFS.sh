c0aae161c7cf9b49cdb4db7e5baaf0a829e98137
JuanSicardoAT <juan.sicardo@agilethought.com>
2021-05-27 18:57:34 -0500

    Hice algo

--- UploadFilesToHDFS.sh
+++ UploadFilesToHDFS.sh
@@ -1,21 +1,21 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-CLOUDERAHOME="/home/cloudera/Documents/Materiales/equipo5/tablasExternas"
-HDFSHOME="/dev/equipo5/clients"
+LOCAL_HOME="/home/cloudera/Documents/Materiales/JediAnalytics"
+HDFS_HOME="/dev/JediAnalytics"
 
-hdfs dfs -mkdir -p ${HDFSHOME}/CAT_TIPO_CLT
-hdfs dfs -put ${CLOUDERAHOME}/datset/CAT_TIPO_CLT.dat ${HDFSHOME}/CAT_TIPO_CLT
-hdfs dfs -mkdir -p ${HDFSHOME}/CAT_TIPO_CONTAC
-hdfs dfs -put ${CLOUDERAHOME}/datset/CAT_TIPO_CONTAC ${HDFSHOME}/CAT_TIPO_CONTAC
-hdfs dfs -mkdir -p ${HDFSHOME}/CAT_TIPO_DOCUM
-hdfs dfs -put ${CLOUDERAHOME}/datset/CAT_TIPO_DOCUM.dat ${HDFSHOME}/CAT_TIPO_DOCUM
-hdfs dfs -mkdir -p ${HDFSHOME}/CLT_CONTACTABILIDAD
-hdfs dfs -put ${CLOUDERAHOME}/datset/CLT_CONTACTABILIDAD.csv ${HDFSHOME}/CLT_CONTACTABILIDAD
-hdfs dfs -mkdir -p ${HDFSHOME}/CLT_DATOS
-hdfs dfs -put ${CLOUDERAHOME}/datset/CLT_DATOS.csv ${HDFSHOME}/CLT_DATOS
-hdfs dfs -mkdir -p ${HDFSHOME}/CLT_DOCUMENTACION
-hdfs dfs -put ${CLOUDERAHOME}/datset/CLT_DOCUMENTACION.csv ${HDFSHOME}/CLT_DOCUMENTACION
-hdfs dfs -mkdir -p ${HDFSHOME}/CLT_RELACION_CTA_CLTE
-hdfs dfs -put ${CLOUDERAHOME}/datset/CLT_RELACION_CTA_CLTE.csv ${HDFSHOME}/CLT_RELACION_CTA_CLTE
-hdfs dfs -mkdir -p ${HDFSHOME}/CLT_SUCURSAL
-hdfs dfs -put ${CLOUDERAHOME}/datset/CLT_SUCURSAL ${HDFSHOME}/CLT_SUCURSAL
+hdfs dfs -mkdir -p ${HDFS_HOME}/CAT_TIPO_CLT
+hdfs dfs -put ${LOCAL_HOME}/datset/CAT_TIPO_CLT.dat ${HDFS_HOME}/CAT_TIPO_CLT
+hdfs dfs -mkdir -p ${HDFS_HOME}/CAT_TIPO_CONTAC
+hdfs dfs -put ${LOCAL_HOME}/datset/CAT_TIPO_CONTAC ${HDFS_HOME}/CAT_TIPO_CONTAC
+hdfs dfs -mkdir -p ${HDFS_HOME}/CAT_TIPO_DOCUM
+hdfs dfs -put ${LOCAL_HOME}/datset/CAT_TIPO_DOCUM.dat ${HDFS_HOME}/CAT_TIPO_DOCUM
+hdfs dfs -mkdir -p ${HDFS_HOME}/CLT_CONTACTABILIDAD
+hdfs dfs -put ${LOCAL_HOME}/datset/CLT_CONTACTABILIDAD.csv ${HDFS_HOME}/CLT_CONTACTABILIDAD
+hdfs dfs -mkdir -p ${HDFS_HOME}/CLT_DATOS
+hdfs dfs -put ${LOCAL_HOME}/datset/CLT_DATOS.csv ${HDFS_HOME}/CLT_DATOS
+hdfs dfs -mkdir -p ${HDFS_HOME}/CLT_DOCUMENTACION
+hdfs dfs -put ${LOCAL_HOME}/datset/CLT_DOCUMENTACION.csv ${HDFS_HOME}/CLT_DOCUMENTACION
+hdfs dfs -mkdir -p ${HDFS_HOME}/CLT_RELACION_CTA_CLTE
+hdfs dfs -put ${LOCAL_HOME}/datset/CLT_RELACION_CTA_CLTE.csv ${HDFS_HOME}/CLT_RELACION_CTA_CLTE
+hdfs dfs -mkdir -p ${HDFS_HOME}/CLT_SUCURSAL
+hdfs dfs -put ${LOCAL_HOME}/datset/CLT_SUCURSAL ${HDFS_HOME}/CLT_SUCURSAL
