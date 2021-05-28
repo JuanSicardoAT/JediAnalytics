d6737601e7d51a4fc16660d0c13e13742b24d94b
JuanSicardoAT <juan.sicardo@agilethought.com>
2021-05-28 17:55:42 -0500

    Se mejoro la ux para subir y borrar todas las tablas externas

--- BashFiles/CreateAllExternalTables.sh
+++ BashFiles/CreateAllExternalTables.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-CREATE_EXTERN_TABLES_HQLS_HOME="HQLs/CreateAllExternalTablesHQLs/"
+CREATE_EXTERN_TABLES_HQLS_HOME="./HQLs/CreateAllExternalTablesHQLs/"
+
+CREATING_ALL_EXT_TABLES_MESSAGE="--Se están creando todas las tablas definidas en:
+  $CREATE_EXTERN_TABLES_HQLS_HOME"
 
 shopt -s nullglob
 
--- BashFiles/SelectAllExternalTables.sh
+++ BashFiles/SelectAllExternalTables.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
-HELPER_HQLS_HOME="HQLs/HelperHQLs/"
+HELPER_HQLS_HOME="./HQLs/HelperHQLs/"
 
+SELECT_ALL_TABLES_MESSAGE="--Se están consultando todas las tablas de jedianalytics"
+
+echo "$SELECT_ALL_TABLES_MESSAGE"
 hive -f "${HELPER_HQLS_HOME}SelectAllExternalTables.hql"
