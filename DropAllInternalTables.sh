453e42a3a39b1e416c15e3bf25a05b74eb121c9d
JuanSicardoAT <84750520+JuanSicardoAT@users.noreply.github.com>
2021-05-28 19:58:49 -0500

    Delete DropAllInternalTables.sh

--- DropAllInternalTables.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-HELPER_HQLS_HOME="./HQLs/HelperHQLs/"
-
-DROPPING_ALL_INT_TABLES_MESSAGE="--Se están borrando todas las tablas internas de jedianalytics"
-
-echo "$DROPPING_ALL_INT_TABLES_MESSAGE"
-hive -f "${HELPER_HQLS_HOME}DropAllInternalTables.hql"
